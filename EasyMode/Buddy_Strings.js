9c5024d19e21101ca078df1dcffc1ce3fa4665e8
jll2884 <simplylopez7@gmail.com>
2019-04-15 11:15:56 -0400

    new updates

---
+++ EasyMode/Buddy_Strings.js
@@ -0,0 +1,6 @@
+var buddyStrings = function(A, B) {
+    if(A==B || A.length==0){
+        return false
+    }
+
+};
---
+++ EasyMode/Word_Pattern.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, str) {
+    pattern = pattern.split('');
+    str = str.split(' ');
+    let bool = true
+    for(word in pattern){
+        console.log(pattern[word]);
+        if(pattern[word]==pattern[word-1] &&word !=0&& str[word]==str[word-1]){
+            return true
+        }
+        else{
+            bool = false
+        }
+    }
+    return bool
+};
+
+console.log(wordPattern("abba","dog cat cat dog"));
