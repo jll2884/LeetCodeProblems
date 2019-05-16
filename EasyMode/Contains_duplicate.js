b59b3dbc874451744f70d521b1adaa731aab2806
jll2884 <simplylopez7@gmail.com>
2019-05-16 00:02:47 -0400

    new updates

---
+++ EasyMode/Contains_duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+        if(Array.from(new Set(nums)).length==nums.length){
+            return false
+        }
+
+    return true
+};
+console.log(containsDuplicate([1,2,2]));
