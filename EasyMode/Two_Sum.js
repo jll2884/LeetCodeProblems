a43ac70c29a970e6bee66451db379d6f1a3c4e03
jll2884 <simplylopez7@gmail.com>
2019-06-17 16:43:04 -0400

    new problems

---
+++ EasyMode/Two_Sum.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+//    must return the index of the numbers in nums list that equal to target value
+    let lst =0
+
+
+};
